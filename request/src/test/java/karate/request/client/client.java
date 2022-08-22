afb0ed6564866e636c096ce0d91cb9c307aa8ebf
Miguel Hugo <dragonmike6@gmail.com>
2022-08-22 09:20:31 -0500

    The code was refactored, we changed the route, etc.

--- request/src/test/java/karate/request/client/client.java
+++ request/src/test/java/karate/request/client/client.java
@@ -1,10 +1,10 @@
-package karate.request;
+package karate.request.client;
 
 import com.intuit.karate.junit5.Karate;
 
 public class client {
     @Karate.Test
     Karate testSample(){
-        return Karate.run("classpath:karate/request/client.feature").relativeTo(getClass());
+        return Karate.run("classpath:karate/request/client_Consult_Get/client.feature").relativeTo(getClass());
     }
 }
--- request/src/test/java/karate/request/create/Create.java
+++ request/src/test/java/karate/request/create/Create.java
@@ -1,10 +1,10 @@
-package karate.request;
+package karate.request.create;
 
 import com.intuit.karate.junit5.Karate;
 
 public class Create {
     @Karate.Test
     Karate testSample(){
-        return Karate.run("classpath:karate/request/create.feature").relativeTo(getClass());
+        return Karate.run("classpath:karate/request/create_A_User_Post/create.feature").relativeTo(getClass());
     }
 }
